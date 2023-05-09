c42ac2eead587aa9fceb4a0ad27054424e59b17f
abiola-sodiq <rasababe01@gmail.com>
2023-05-09 14:15:14 +0100

    first commmit

---
+++ main.js
@@ -0,0 +1,10 @@
+let questions = [
+    {
+        question: "What is 2+2 ?",
+        answers:[
+            {text: "six",correct:false},
+            {text: "four",correct:true}
+        ]
+
+    }
+]
